57f78e5b67738e3a20b1d5be54415fa13bff79c4
nikolapajovic-bit <nikpajovic@gmail.com>
2026-08-18 23:25:22 +0200

    some config fixes

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
+const BACKEND_URL = process.env.BACKEND_URL ?? "http://localhost:5000";
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
@@ -8,6 +10,18 @@ const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.100.6"],
   turbopack: {
     root: __dirname,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${BACKEND_URL}/api/:path*`,
+      },
+      {
+        source: "/uploads/:path*",
+        destination: `${BACKEND_URL}/uploads/:path*`,
+      },
+    ];
   },
 };
 
